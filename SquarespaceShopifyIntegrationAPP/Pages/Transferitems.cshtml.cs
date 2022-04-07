23a4f11aec0df2e533b2c832eb256ff0609a7311
morton_al@hotmail.com <morton_al@hotmail.com>
2022-04-07 22:07:25 +0900

    Changed model on Transferitems page. CSS changes

--- SquarespaceShopifyIntegrationAPP/Pages/Transferitems.cshtml.cs
+++ SquarespaceShopifyIntegrationAPP/Pages/Transferitems.cshtml.cs
@@ -6,27 +6,49 @@ namespace SquarespaceShopifyIntegrationAPP.Pages
 {
     public class TransferitemsModel : PageModel
     {
-        public IEnumerable<string> ArtistPictureLinks { get; private set; }
+        public IEnumerable<CollectionItem> ArtistPictureLinks { get; set; }
+
+        public TransferitemsModel()
+        {
+           ArtistPictureLinks = new List<CollectionItem>();
+        }
+
         public void OnGet(string url)
         {
-            ArtistPictureLinks = new List<string>();
-
             url = Uri.UnescapeDataString(url);
-
             var web = new HtmlWeb();
             var doc = web.Load(url);
 
             ArtistPictureLinks = doc.DocumentNode.SelectNodes("//a")
-            .Where(a => a.Attributes.Any(attr => a.HasClass("grid-item")
-            ))
-            .SelectMany(a => a.ChildNodes.SelectMany(c => c.ChildNodes.Where(a => a.HasClass("grid-image-inner-wrapper"))
-            .SelectMany(a => a.ChildNodes.Where(a => a.Name == "img").Select(imgNode =>
-            {
-                var src = imgNode.GetAttributeValue("data-src", "");
-                imgNode.Attributes.Add("src", src);
-                return imgNode;
-            }))))
-            .Select(pl => pl.ParentNode.ParentNode.ParentNode.OuterHtml);
+                .Where(a => a.Attributes.Any(attr => a.HasClass("grid-item")))
+                .Where(n => n.ChildNodes.Any(a => a.HasClass("grid-image") && a.ChildNodes.Any(a => a.HasClass("grid-image-inner-wrapper") && a.ChildNodes.Any(a => a.Name == "img"))))
+                .Select(n =>
+                {
+
+                    var imageUrl = n.ChildNodes.First(a => a.HasClass("grid-image")).ChildNodes.First(a => a.HasClass("grid-image-inner-wrapper"))
+                     .ChildNodes.First(a => a.Name == "img").GetAttributeValue("data-src", "");
+                    var src = n.GetAttributeValue("href", "");
+                    var name = n.ChildNodes.First(a => a.ChildNodes.Any(a => a.HasClass("portfolio-title"))).ChildNodes.First(n => n.Name == "h3").InnerText;
+
+                    return new CollectionItem
+                    {
+                        ImgSrc = imageUrl,
+                        ItemUrl = src,
+                        Name = name
+                    };
+                });
+        }
+    }
+
+    public class CollectionItem
+    {
+        public string ItemUrl { get; set; }
+        public string ImgSrc { get; set; }
+        public string Name { get; set; }
+
+        public string GetImageWithSize(int size)
+        {
+            return ImgSrc + $"?format={size}w";
         }
     }
 }
